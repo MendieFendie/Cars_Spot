ef20880a9f1cc78daebfece38538d1df2ac87fa1
Oleh Savaryn <olegsavaryn@gmail.com>
2024-09-10 22:03:15 +0200

    add slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import SimpleSlider from "./slider/slider";
 import stroke from "./icons/Stroke.svg";
 import logo from "./icons/CTA.svg";
+import { useState } from "react";
 
 function App() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
     <div className="bg-customGray max-w-[1440px] relative  custom-bg-lines z-10">
       <header className="w-full h-[79px] flex items-center border border-red-500">
@@ -13,21 +23,21 @@ function App() {
           </div>
           <div className="flex  ">
             <p className="font-robotoFlex text-base mr-[24px]">Galeria zdjęć</p>
-            <p className="font-robotoFlex text-base">FaQ</p>
+            <p className="font-robotoFlex text-base ">FaQ</p>
           </div>
           <button className="w-[157px] h-[47px] bg-customBlue rounded-lg">
-            <p className=" font-robotoFlex text-base"> Zadzwoń do nas</p>
+            <p className=" font-robotoFlex text-base "> Zadzwoń do nas</p>
           </button>
         </div>
       </header>
       <main className=" relative  custom-bg-cars">
-        <section className="container h-[825px] mx-auto border border-red-500">
-          <div className="pt-[120px] pl-[120px] pr-[120px]">
+        <section className="container mx-auto border border-red-500">
+          <div className="pt-[120px] pl-[120px] pr-[120px] pb-[362px]">
             <div className=" mb-12">
-              <h2 className=" font-bebasNeue text-[4.77rem] mb-8 leading-[1.1]">
+              <h2 className=" font-bebasNeue text-[4.77rem] mb-8 leading-[1.1] tracking-tight">
                 Sprzedajemy samochody <br /> z europy
               </h2>
-              <p className=" text-base">
+              <p className=" text-base leading-normal">
                 Kup komfortowy pojazd, aby każda podróż <br /> była wyjątkowym
                 przeżyciem.
               </p>
@@ -42,17 +52,79 @@ function App() {
             </div>
           </div>
         </section>
-        <section className="container mx-auto h-[207p] pt-20 ">
-          <div>
-            <h2>Prezentacja firmy</h2>
-            <p>Zobacz naszą Galerię zdjęć</p>
+        <section className="container mx-auto pt-20 border border-red-500">
+          <div className="border border-red-500 mb-6">
+            <h2 className="font-robotoCondensed font-light text-[21.5px] text-customBlue ">
+              Prezentacja firmy
+            </h2>
+            <p className=" font-bebasNeue text-[40px] leading-tight">
+              Zobacz naszą Galerię zdjęć
+            </p>
           </div>
-          <div>
-            <p>Samochody osobowe</p>
-            <p>Samochody dostawcze</p>
+          <div className="flex border border-red-500">
+            <p className=" text-customBlue font-robotoFlex font-semibold text-base mr-12">
+              Samochody osobowe
+            </p>
+            <p className="font-robotoFlex text-base">Samochody dostawcze</p>
           </div>
         </section>
-        <section></section>
+        <section className=" w-full mx-auto pl-[120px] pt-20 pb-[152px] ">
+          <SimpleSlider />
+        </section>
+        <section className=" flex container mx-auto px-[120px]">
+          <div className=" mr-12">
+            <h2 className=" font-bebasNeue text-[25px] mb-1">
+              Id nostrud enim esse consectetur in eu mollit fugiat aute. Velit
+              nulla commodo.
+            </h2>
+            <div className="relative">
+              <p
+                className={` overflow-hidden transition-all duration-300 ${isExpanded ? "max-h-[500px]" : "max-h-[78px]"}`}
+              >
+                Mauris varius ipsum mauris, rutrum lobortis magna efficitur a.
+                Donec egestas, nisl vehicula feugiat ornare, diam turpis
+                efficitur mi, ac venenatis eros ipsum ac arcu. Vestibulum et
+                lacus sit amet erat vestibulum malesuada. Curabitur lobortis
+                augue at sapien placerat, vitae varius sapien scelerisque.
+                Aliquam erat volutpat. Donec sit amet sapien at odio ullamcorper
+                vulputate. In in ligula id lectus posuere vestibulum...
+              </p>
+
+              <button
+                onClick={toggleText}
+                className="text-customBlue mt-2 font-semibold"
+              >
+                {isExpanded ? "Zwiń" : "Rozwiń"}
+              </button>
+            </div>
+          </div>
+          <div>
+            <h2 className=" font-bebasNeue text-[25px] mb-1">
+              Id nostrud enim esse consectetur in eu mollit fugiat aute. Velit
+              nulla commodo.
+            </h2>
+            <div className="relative">
+              <p
+                className={` overflow-hidden transition-all duration-300 ${isExpanded ? "max-h-[500px]" : "max-h-[78px]"}`}
+              >
+                Mauris varius ipsum mauris, rutrum lobortis magna efficitur a.
+                Donec egestas, nisl vehicula feugiat ornare, diam turpis
+                efficitur mi, ac venenatis eros ipsum ac arcu. Vestibulum et
+                lacus sit amet erat vestibulum malesuada. Curabitur lobortis
+                augue at sapien placerat, vitae varius sapien scelerisque.
+                Aliquam erat volutpat. Donec sit amet sapien at odio ullamcorper
+                vulputate. In in ligula id lectus posuere vestibulum...
+              </p>
+
+              <button
+                onClick={toggleText}
+                className="text-customBlue mt-2 font-semibold"
+              >
+                {isExpanded ? "Zwiń" : "Rozwiń"}
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
 
       <footer></footer>
---
+++ src/slider/slider.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Slider from "react-slick";
+import "./slider.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import cars1 from "../img/cars/Images-1.jpg";
+import cars2 from ".././img/cars/Images-2.jpg";
+import cars3 from "../img/cars/Images.jpg";
+
+const SimpleSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2.1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0",
+    with: 1440,
+    swipeToSlide: true,
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <img className="slider-img" src={cars1} alt="Car 1" />
+      </div>
+      <div>
+        <img className="slider-img" src={cars3} alt="Car 2" />
+      </div>
+      <div>
+        <img className="slider-img" src={cars2} alt="Car 3" />
+      </div>
+    </Slider>
+  );
+};
+
+export default SimpleSlider;
